960d0f758ba5487be9a499618c47c05a8125a10a
safvanpk1998 <68804604+safvanpk1998@users.noreply.github.com>
2022-12-15 14:34:57 +0530

    de

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -50,67 +50,68 @@ useEffect(() => {
 
   // }, [dispatch,products.productCount]);
   return (
-    <div className="slider">
-      <div className="header">
-        <div className="time">
-          <Title level={3}>{heading}</Title>
-          {timer ? (
-            <div className="timer">
-              Ends in <Countdown date={Date.now() + 100000000} precision={3} />
-            </div>
-          ) : null}
+    <></>
+    // <div className="slider">
+    //   <div className="header">
+    //     <div className="time">
+    //       <Title level={3}>{heading}</Title>
+    //       {timer ? (
+    //         <div className="timer">
+    //           Ends in <Countdown date={Date.now() + 100000000} precision={3} />
+    //         </div>
+    //       ) : null}
 
-          <div className="view">View All</div>
-        </div>
+    //       <div className="view">View All</div>
+    //     </div>
 
-        <hr></hr>
+    //     <hr></hr>
 
-        <ScrollMenu
-          arrowLeft={<div style={{ fontSize: "30px" }}>{" < "}</div>}
-          arrowRight={<div style={{ fontSize: "30px" }}>{" > "}</div>}
-        />
-      </div>
-      <div className="product-scroll">
-        {productData&&productData.products?
-        <>
-        {productData.products?.map((e) => {
-          return (
-            <>
-            <Col className="col" key={e._id}>
-              <Link
-                key={e._id}
-                className="productCard"
-                to={`/productDetails/${e._id}`}
-              >
-                <Badge.Ribbon text={`${e.offer}% off`} color="yellow">
-                <Card bordered={true}>
-                  <div className="product-card">
-                    <div>
-                      {/* <img src={speaker} alt="" /> */}
-                      <img src={e.image[0].url} alt="product Image" />
-                    </div>
-                  </div>
-                  <div className="deatails">
-                    <Title level={5}>{e.name}</Title>
-                    <span>
-                      <Rate allowHalf disabled defaultValue={e.ratings} />{" "}
-                    </span>
-                    <span>({e.numberofReviews})</span>
-                    <div className="rate">
-                      <Title level={5}>
-                        <FaRupeeSign /><span className="ogPrice">{e.price}</span>  {e.amountPayable} Only
-                      </Title>
-                    </div>
-                  </div>
-                </Card></Badge.Ribbon>
-              </Link>
-            </Col>
+    //     <ScrollMenu
+    //       arrowLeft={<div style={{ fontSize: "30px" }}>{" < "}</div>}
+    //       arrowRight={<div style={{ fontSize: "30px" }}>{" > "}</div>}
+    //     />
+    //   </div>
+    //   <div className="product-scroll">
+    //     {productData&&productData.products?
+    //     <>
+    //     {productData.products?.map((e) => {
+    //       return (
+    //         <>
+    //         <Col className="col" key={e._id}>
+    //           <Link
+    //             key={e._id}
+    //             className="productCard"
+    //             to={`/productDetails/${e._id}`}
+    //           >
+    //             <Badge.Ribbon text={`${e.offer}% off`} color="yellow">
+    //             <Card bordered={true}>
+    //               <div className="product-card">
+    //                 <div>
+    //                   {/* <img src={speaker} alt="" /> */}
+    //                   <img src={e.image[0].url} alt="product Image" />
+    //                 </div>
+    //               </div>
+    //               <div className="deatails">
+    //                 <Title level={5}>{e.name}</Title>
+    //                 <span>
+    //                   <Rate allowHalf disabled defaultValue={e.ratings} />{" "}
+    //                 </span>
+    //                 <span>({e.numberofReviews})</span>
+    //                 <div className="rate">
+    //                   <Title level={5}>
+    //                     <FaRupeeSign /><span className="ogPrice">{e.price}</span>  {e.amountPayable} Only
+    //                   </Title>
+    //                 </div>
+    //               </div>
+    //             </Card></Badge.Ribbon>
+    //           </Link>
+    //         </Col>
            
-            </>
-          );
-        })}</>:<><Skeleton active /></>}
-      </div>
-    </div>
+    //         </>
+    //       );
+    //     })}</>:<><Skeleton active /></>}
+    //   </div>
+    // </div>
   );
 }
 
