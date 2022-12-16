6a64d9b7adbcbc04a8a81da500b09b6533fdd30b
safvanpk1998 <68804604+safvanpk1998@users.noreply.github.com>
2022-12-16 11:58:56 +0530

    image

--- src/components/Product-deatail Component/BasicDetails.js
+++ src/components/Product-deatail Component/BasicDetails.js
@@ -1,6 +1,6 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Link,useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import {
   Menu,
   Input,
@@ -10,10 +10,10 @@ import {
   Button,
   Rate,
   Tabs,
-  Empty ,
+  Empty,
   Table,
-  Skeleton ,
-  message
+  Skeleton,
+  message,
 } from "antd";
 import { MinusOutlined, PlusOutlined } from "@ant-design/icons";
 
@@ -23,27 +23,27 @@ import jbl from "../../assets/images/jbl.png";
 import "./basicDetails.scss";
 import Review from "./review/review";
 import Slider from "../slider/Slider";
-import {getProduct} from "../../slices/productSlice"
-import {getProductDeatails} from "../../slices/productDetailSlice"
-import { createNewWishList, clearWishListError } from "../../slices/wishListSlice";
+import { getProduct } from "../../slices/productSlice";
+import { getProductDeatails } from "../../slices/productDetailSlice";
+import {
+  createNewWishList,
+  clearWishListError,
+} from "../../slices/wishListSlice";
 const { Title } = Typography;
 
 function BasicDetails() {
   const dispatch = useDispatch();
- 
- 
+
   const { id } = useParams();
-  const productDetail = useSelector((state) => state.productDetails.productDetails);
-  const data=productDetail.products
-  const { wishList, loading, itemdeleted,productadded,err} = useSelector(
+  const productDetail = useSelector(
+    (state) => state.productDetails.productDetails
+  );
+  const data = productDetail.products;
+  const { wishList, loading, itemdeleted, productadded, err } = useSelector(
     (state) => state.wishList
   );
-  
-  
- 
-  const onChange = (value) => {
- 
-  };
+
+  const onChange = (value) => {};
   const [tableParams, setTableParams] = useState({
     pagination: {
       current: 1,
@@ -53,21 +53,20 @@ function BasicDetails() {
   if (productadded == true) {
     message.success("Product Added To WishList");
 
-    dispatch(clearWishListError())
+    dispatch(clearWishListError());
   }
   if (productadded == false) {
     message.error(err.message);
 
-    dispatch(clearWishListError())
+    dispatch(clearWishListError());
   }
   // if (err) {
   //   message.error("something went wrong! please try again");
   // }
 
-
-  const addtoWishList=()=>{
-    dispatch(createNewWishList(id))
-  }
+  const addtoWishList = () => {
+    dispatch(createNewWishList(id));
+  };
   const [count, setCount] = useState(1);
   const add = () => {
     let newcount = count + 1;
@@ -121,252 +120,258 @@ function BasicDetails() {
     // dispatch(getProduct(id));
     dispatch(getProductDeatails(id));
     window.scrollTo(0, 0);
- 
-    
-  }, [dispatch,id]);
- 
+  }, [dispatch, id]);
+
   return (
     <>
-
-   
-   
- 
- 
-    <div className="BasicDetails">
-       {productDetail&& data? 
-      <div className="header">
-        <div className="title">
-          <Title level={3}>
-          {data.name} 
-          </Title>
-          <div className="info">
-            <div className="data">
-              <p>
-                <span className="brand"> Brand : </span>
-                <span className="name"> {data.brand} </span>
-              </p>
-              <div className="vertical"></div>
+      <div className="BasicDetails">
+        {productDetail && data ? (
+          <div className="header">
+            <div className="title">
+              <Title level={3}>{data.name}</Title>
+              <div className="info">
+                <div className="data">
+                  <p>
+                    <span className="brand"> Brand : </span>
+                    <span className="name"> {data.brand} </span>
+                  </p>
+                  <div className="vertical"></div>
+                </div>
+                <div className="data">
+                  <p>
+                    <span>
+                      <Rate allowHalf disabled defaultValue={data.ratings} />{" "}
+                    </span>
+                    <span>{`(${data.numberofReviews} Reviews)`}</span>
+                  </p>
+                  <div className="vertical"></div>
+                </div>
+                <div className="data">
+                  <p>
+                    <span>SKU :</span>
+                    <span className="name">CD 7824593651</span>
+                  </p>
+                </div>
+              </div>
             </div>
-            <div className="data">
-              <p>
-                <span>
-                  <Rate allowHalf disabled defaultValue= {data.ratings}  />{" "}
-                </span>
-                <span>{`(${data.numberofReviews} Reviews)`}</span>
-              </p>
-              <div className="vertical"></div>
-            </div>
-            <div className="data">
-              <p>
-                <span>SKU :</span>
-                <span className="name">CD 7824593651</span>
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="add">
-          <div className="count">
-          <Button.Group>
-          <Button onClick={minus} icon={<MinusOutlined />} />
-           
-          
-           <Title level={3}>
-               {count}
-             </Title>
-             <Button onClick={add} icon={<PlusOutlined />} />
+            <div className="add">
+              <div className="count">
+                <Button.Group>
+                  <Button onClick={minus} icon={<MinusOutlined />} />
 
-          
-         </Button.Group>
+                  <Title level={3}>{count}</Title>
+                  <Button onClick={add} icon={<PlusOutlined />} />
+                </Button.Group>
+              </div>
+              <div className="buy">
+                <div className="cart-button">
+                  <Button onClick={addtoWishList}> add to cart</Button>
+                </div>
 
-          </div>
-         <div className="buy">
-         <div className="cart-button">
-            <Button onClick={addtoWishList}> add to cart</Button>
-          </div>
- 
-
-           <Link to={`/product/productdetails/checkout/${id}/${count}`}> <Button>Buy</Button></Link>
-
-         </div>
-         
-        </div>
-        <div className="info-medium">
-            <div className="data">
-              <p>
-                <span className="brand"> Brand : </span>
-                <span className="name"> {data.brand} </span>
-              </p>
-              <div className="vertical"></div>
+                <Link to={`/product/productdetails/checkout/${id}/${count}`}>
+                  {" "}
+                  <Button>Buy</Button>
+                </Link>
+              </div>
             </div>
-            <div className="data">
-              <p>
-                <span>
-                  <Rate allowHalf disabled defaultValue= {data.ratings}  />{" "}
-                </span>
-                <span>{` (${data.numberofReviews}Reviews)`}</span>
-              </p>
-              <div className="vertical"></div>
-            </div>
-            <div className="data">
-              <p>
-                <span>SKU :</span>
-                <span className="name">CD 7824593651</span>
-              </p>
-            </div>
-          </div>
-      </div>:<><Skeleton active /></>}
-      <hr></hr>
-      
-      <div className="summary">
-        <div className="image">
-          <div className="pics"></div>
-          <div className="enlarge">
-            <MagnifierContainer>
-              <GlassMagnifier
-               imageSrc={data.image[0].url} 
-             
-                //  imageSrc={e.image[0].url} 
-
-                magnifierSize="80%"
-                allowOverflow="false"
-                style={{width: "80%"}}
-                
-              />
-            </MagnifierContainer>
-          </div>
-          <div className="title-Screen">
-        
-            <div className="rate">
-              <Title level={3}>
-                <FaRupeeSign size={20}></FaRupeeSign>    {data.amountPayable} 
-              </Title>
-              <p className="dashed">
-                <FaRupeeSign size={13}></FaRupeeSign>{" "}
-                <span className="old-value">    {data.price} </span>
-              </p>
-              <Title level={5}>(   {data.offer}%off )</Title>
-            </div>
-
-            <div className="dealer">
-              <p>
-                <span className="brand"> Sold By : </span>
-                <span className="name"> {data.soldBy.name}</span>
+            <div className="info-medium">
+              <div className="data">
+                <p>
+                  <span className="brand"> Brand : </span>
+                  <span className="name"> {data.brand} </span>
+                </p>
                 <div className="vertical"></div>
-                <span className="brand"> Status : </span>
-                <span className="name">    {data.stock>0?"In stock":"Out Of Stock"} </span>
-              </p>
-              <p>Inclusive of all taxes</p>
-      
-          </div>
-            </div>
-
-        </div>
-        {productDetail&& data? 
-        <div className="details">
-          <div className="title" >
-            <div className="rate">
-              <Title level={3}>
-                <FaRupeeSign size={20}></FaRupeeSign>    {data.amountPayable} 
-              </Title>
-              <p className="dashed">
-                <FaRupeeSign size={13}></FaRupeeSign>{" "}
-                <span className="old-value">    {data.price} </span>
-              </p>
-              <Title level={5}>(   {data.offer}%off )</Title>
-            </div>
-
-            <div className="dealer">
-              <p>
-                <span className="brand"> Sold By : </span>
-                <span className="name"> {data.soldBy.name}</span>
+              </div>
+              <div className="data">
+                <p>
+                  <span>
+                    <Rate allowHalf disabled defaultValue={data.ratings} />{" "}
+                  </span>
+                  <span>{` (${data.numberofReviews}Reviews)`}</span>
+                </p>
                 <div className="vertical"></div>
-                <span className="brand"> Status : </span>
-                <span className="name">    {data.stock>0?"In stock":"Out Of Stock"} </span>
-              </p>
-              <p>Inclusive of all taxes</p>
+              </div>
+              <div className="data">
+                <p>
+                  <span>SKU :</span>
+                  <span className="name">CD 7824593651</span>
+                </p>
+              </div>
             </div>
           </div>
-          <hr />
-          <div className="list">
-            <ul class="a">
-            {data.specs?.map((e) => {
-          return (
-            <li>{e}</li>
+        ) : (
+          <>
+            <Skeleton active />
+          </>
+        )}
+        <hr></hr>
 
-          )})}
-              
-              {/* <li>Free from the confines of wires and chords</li>
+        <div className="summary">
+          <div className="image">
+            <div className="pics"></div>
+            <div className="enlarge">
+              {data.image.length > 0 ? (
+                <MagnifierContainer>
+                  <GlassMagnifier
+                    imageSrc={data.image[0].url}
+                    //  imageSrc={e.image[0].url}
+
+                    magnifierSize="80%"
+                    allowOverflow="false"
+                    style={{ width: "80%" }}
+                  />
+                </MagnifierContainer>
+              ) : (
+                <></>
+              )}
+            </div>
+            <div className="title-Screen">
+              <div className="rate">
+                <Title level={3}>
+                  <FaRupeeSign size={20}></FaRupeeSign> {data.amountPayable}
+                </Title>
+                <p className="dashed">
+                  <FaRupeeSign size={13}></FaRupeeSign>{" "}
+                  <span className="old-value"> {data.price} </span>
+                </p>
+                <Title level={5}>( {data.offer}%off )</Title>
+              </div>
+
+              <div className="dealer">
+                <p>
+                  <span className="brand"> Sold By : </span>
+                  <span className="name"> {data.soldBy.name}</span>
+                  <div className="vertical"></div>
+                  <span className="brand"> Status : </span>
+                  <span className="name">
+                    {" "}
+                    {data.stock > 0 ? "In stock" : "Out Of Stock"}{" "}
+                  </span>
+                </p>
+                <p>Inclusive of all taxes</p>
+              </div>
+            </div>
+          </div>
+          {productDetail && data ? (
+            <div className="details">
+              <div className="title">
+                <div className="rate">
+                  <Title level={3}>
+                    <FaRupeeSign size={20}></FaRupeeSign> {data.amountPayable}
+                  </Title>
+                  <p className="dashed">
+                    <FaRupeeSign size={13}></FaRupeeSign>{" "}
+                    <span className="old-value"> {data.price} </span>
+                  </p>
+                  <Title level={5}>( {data.offer}%off )</Title>
+                </div>
+
+                <div className="dealer">
+                  <p>
+                    <span className="brand"> Sold By : </span>
+                    <span className="name"> {data.soldBy.name}</span>
+                    <div className="vertical"></div>
+                    <span className="brand"> Status : </span>
+                    <span className="name">
+                      {" "}
+                      {data.stock > 0 ? "In stock" : "Out Of Stock"}{" "}
+                    </span>
+                  </p>
+                  <p>Inclusive of all taxes</p>
+                </div>
+              </div>
+              <hr />
+              <div className="list">
+                <ul class="a">
+                  {data.specs?.map((e) => {
+                    return <li>{e}</li>;
+                  })}
+
+                  {/* <li>Free from the confines of wires and chords</li>
               <li>20 hours of portable capabilities</li>
               <li>Double-ended Coil Cord with 3.5mm Stereo Plugs Included</li>
               <li>3/4″ Dome Tweeters: 2X and 4″ Woofer: 1X</li> */}
-            </ul>
-          </div>
-        </div>:<><Skeleton active /></>}
-      </div>
-      <div className="tabs">
-      {productDetail&& data? 
-        <Tabs defaultActiveKey="1">
-          <Tabs.TabPane tab="Discription" key="1">
-          {data.discription?
-            <div>
-              <div>
-                <Title level={4}>
-                  Embodying the Raw, Wayward Spirit of Rock ‘N’ Roll
-                </Title>
+                </ul>
               </div>
-              <div>
-                <p>
-               {data.discription}
-                </p>
-              </div>
-            </div>:<div><Empty /></div>}
-          </Tabs.TabPane>
-          <Tabs.TabPane tab="Specifications" key="2">
-            <Table
-              dataSource={dataSource}
-              columns={columns}
-              pagination={tableParams.pagination}
-            />
-            ;
-          </Tabs.TabPane>
-          <Tabs.TabPane tab="Reviews" key="3">
-            <Review></Review>
-          </Tabs.TabPane>
-          <Tabs.TabPane tab="Vendor Info" key="4">
-         
-
-            <div className="vendor">
-              <Title level={4}>Vendor Information</Title>
             </div>
-            {productDetail&& data? 
-            <div className="vendor">
-              <p>
-                {" "}
-                <span className="criteria"> Store Name : </span>New Delhi
-                Collections{" "}
-              </p>
-              <p>
-                {" "}
-                <span className="criteria"> Vendor : </span> New Delhi
-                Collections
-              </p>
-              <p>
-                {" "}
-                <span className="criteria"> Address: </span> 335 Orchard , Navi
-                Mumbai
-              </p>
-             
-              <p>{`${data.ratings} rating From ${data.numberofReviews} Users`}</p>
-              <Rate allowHalf disabled defaultValue={data.ratings} />{" "}
-            </div>:<><Empty></Empty></>}
-          </Tabs.TabPane>
-        </Tabs>:<><Skeleton active /></>}
+          ) : (
+            <>
+              <Skeleton active />
+            </>
+          )}
+        </div>
+        <div className="tabs">
+          {productDetail && data ? (
+            <Tabs defaultActiveKey="1">
+              <Tabs.TabPane tab="Discription" key="1">
+                {data.discription ? (
+                  <div>
+                    <div>
+                      <Title level={4}>
+                        Embodying the Raw, Wayward Spirit of Rock ‘N’ Roll
+                      </Title>
+                    </div>
+                    <div>
+                      <p>{data.discription}</p>
+                    </div>
+                  </div>
+                ) : (
+                  <div>
+                    <Empty />
+                  </div>
+                )}
+              </Tabs.TabPane>
+              <Tabs.TabPane tab="Specifications" key="2">
+                <Table
+                  dataSource={dataSource}
+                  columns={columns}
+                  pagination={tableParams.pagination}
+                />
+                ;
+              </Tabs.TabPane>
+              <Tabs.TabPane tab="Reviews" key="3">
+                <Review></Review>
+              </Tabs.TabPane>
+              <Tabs.TabPane tab="Vendor Info" key="4">
+                <div className="vendor">
+                  <Title level={4}>Vendor Information</Title>
+                </div>
+                {productDetail && data ? (
+                  <div className="vendor">
+                    <p>
+                      {" "}
+                      <span className="criteria"> Store Name : </span>New Delhi
+                      Collections{" "}
+                    </p>
+                    <p>
+                      {" "}
+                      <span className="criteria"> Vendor : </span> New Delhi
+                      Collections
+                    </p>
+                    <p>
+                      {" "}
+                      <span className="criteria"> Address: </span> 335 Orchard ,
+                      Navi Mumbai
+                    </p>
+                    <p>{`${data.ratings} rating From ${data.numberofReviews} Users`}</p>
+                    <Rate allowHalf disabled defaultValue={data.ratings} />{" "}
+                  </div>
+                ) : (
+                  <>
+                    <Empty></Empty>
+                  </>
+                )}
+              </Tabs.TabPane>
+            </Tabs>
+          ) : (
+            <>
+              <Skeleton active />
+            </>
+          )}
+        </div>
+        <div></div>
       </div>
-      <div>
-       
-      </div>
-    </div>
-   </>
+    </>
   );
 }
 
