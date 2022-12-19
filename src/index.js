49588f06c7b35ed53c8ba81d660a43c978bf38e2
safvanpk1998 <68804604+safvanpk1998@users.noreply.github.com>
2022-12-19 11:02:00 +0530

    404

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ root.render(
  <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
       
-    <Router>
+    <Router basename="/THE_MARKET_CLIENT-">
     <App />
   </Router>
   </PersistGate>
