69cdb0d1b4aeddf22c7508b8e2c2e992fbf76f71
safvanpk1998 <68804604+safvanpk1998@users.noreply.github.com>
2023-02-03 17:37:05 +0530

    local branch

--- src/components/home-component/Home.js
+++ src/components/home-component/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { BackTop } from 'antd';
+import { BackTop } from "antd";
 import Banner from "../banner-components/Banner";
 import Policy from "../banner-components/Policy";
 import Homeposter from "../banner-components/posters/Home-poster";
@@ -9,30 +9,24 @@ import Product from "../product-component/product";
 import ProductDetails from "../product-component/Product-Details";
 import Slider from "../slider/Slider";
 import Timer from "../timer/timer";
-import Footer from "../layout-component/footer"
+import Footer from "../layout-component/footer";
 function Home() {
   return (
     <div>
-  
-        <Header></Header>
-        <Banner></Banner>
-        <Policy></Policy>
-        <Homeposter></Homeposter>
-        <Slider heading="Deal Of The Day" timer={true} ></Slider>
-        <Slider heading="Electronics $ Mobiles" keyword={"apple"}></Slider>
-        <Slider heading="Home & Kitchen Essentials" keyword={"nokia"}></Slider>
-        <Slider heading="Sports, Healthcare & more" keyword={"Electronics"}></Slider>
-        <Slider heading="Beauty, Food, Toys & more"></Slider>
-      
-        <Footer></Footer>
-        
+      <Header></Header>
+      <Banner></Banner>
+      <Policy></Policy>
+      <Homeposter></Homeposter>
+      <Slider heading="Deal Of The Day" timer={true}></Slider>
+      <Slider heading="Electronics $ Mobiles" keyword={"apple"}></Slider>
+      <Slider heading="Home & Kitchen Essentials" keyword={"nokia"}></Slider>
+      <Slider
+        heading="Sports, Healthcare & more"
+        keyword={"Electronics"}
+      ></Slider>
+      <Slider heading="Beauty, Food, Toys & more"></Slider>
 
-        
-        
-       
-    
-     
-
+      <Footer></Footer>
     </div>
   );
 }
